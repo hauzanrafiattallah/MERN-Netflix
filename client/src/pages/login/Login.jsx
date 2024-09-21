6c40cf8547c26df19906457a6e55891c802ce2c9
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-09-22 01:37:40 +0700

    Update Netflix logo source in Login component

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
           <Link to="/register">
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+              src="/netflix.png"
               alt=""
             />
           </Link>
